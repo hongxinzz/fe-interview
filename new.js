35f481b79acae2bf0a180f2f9a1531a6e5590f0e
hongxinzz <510348414@qq.com>
2020-11-03 21:59:17 +0800

    :sparkles: new

--- new.js
+++ new.js
@@ -3,11 +3,11 @@
  * @LastEditors: xinghe
  * @Date: 2020-10-30 22:37:20
  * @FilePath: /fe-interview/new.js
- * @LastEditTime: 2020-11-03 11:55:02
+ * @LastEditTime: 2020-11-03 21:59:01
  * @symbol_custom_string_obkoro: 不想有bug xinghe@gaoding.com
  */
 function myNew(target, ...args) {
   let obj = Object.create(target.prototype);
-  let result = obj.apply(target, args);
+  let result = target.apply(obj, args);
   return typeof result === "object" ? result : obj;
 }
